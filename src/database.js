8d7155a78df9562d9de80c0cb9664066a42e292a
borjadb <you@example.com>
2020-06-15 21:10:24 +0200

    config heroku

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/notedb', {
+
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/notes-db', {
 	useCreateIndex: true,
 	useNewUrlParser: true,
 	useFindAndModify: false
